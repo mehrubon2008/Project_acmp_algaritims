87aca1dfd46cef9a5df09fe95986b948d28077f9
mehrubonpy <124867529+mehrubonpy@users.noreply.github.com>
2023-11-22 08:33:10 +0500

    hello world

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('hello world')
+s1 = input()
+s2 = input()
+s3 = input()
+st = min(len(s1), len(s2), len(s3))
+f = 0
+k = 0
+for i in range(st):
+    if s1[i] == s2[i] and s2[i] == s3[i]:
+        f = 1
+    else:
+        k = i
+        break
+if f == 0:
+    print(-1)
+else:
+    print( max(len(s1), len(s2), len(s3)) - st)
